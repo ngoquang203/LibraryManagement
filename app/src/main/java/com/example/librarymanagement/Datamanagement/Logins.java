87c95f1d056f2d79a878c158afc4fe06eed9fa42
Ngo Xuan Quang <ngoquang3782@gmail.com>
2024-05-22 13:20:54 +0700

    update icon, sqlserver

---
+++ app/src/main/java/com/example/librarymanagement/Datamanagement/Logins.java
@@ -0,0 +1,52 @@
+package com.example.librarymanagement.Datamanagement;
+
+import com.example.librarymanagement.SQLServerHelper.SQLmanagement;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class Logins {
+    String User;
+    String pass;
+    public Logins(){};
+    public Logins(String user, String pass) {
+        User = user;
+        this.pass = pass;
+    }
+
+    public static ArrayList<Logins> getuserlist() throws SQLException {
+        Connection connection = SQLmanagement.connectionSQLSever();
+        ArrayList<Logins> list = new ArrayList<>();
+        Statement statement = connection.createStatement();// Tạo đối tượng Statement.
+        String sql = "select * from Logins";
+        // Thực thi câu lệnh SQL trả về đối tượng ResultSet. // Mọi kết quả trả về sẽ được lưu trong ResultSet
+        ResultSet rs = statement.executeQuery(sql);
+        while (rs.next()) {
+            list.add(new Logins(
+                    rs.getString("ID").trim(),
+                    rs.getString("Passwords").trim()));// Đọc dữ liệu từ ResultSet
+        }
+        statement.close();
+        connection.close();// Đóng kết nối
+        return list;
+    }
+
+    public String getUser() {
+        return User;
+    }
+
+    public void setUser(String user) {
+        User = user;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+}
---
+++ app/src/main/java/com/example/librarymanagement/SQLServerHelper/SQLmanagement.java
@@ -0,0 +1,30 @@
+package com.example.librarymanagement.SQLServerHelper;
+
+import android.os.StrictMode;
+import android.util.Log;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class SQLmanagement {
+    private static String sql = "jdbc:jtds:sqlserver://192.168.60.237:1433;databasename=test;user=dataCNPM;password=ngoquanghy123"; // dia chi ket noi
+    public static Connection connectionSQLSever(){
+        Connection connection = null;
+        try {
+            try {
+                StrictMode.ThreadPolicy policy =
+                        new StrictMode.ThreadPolicy.Builder().permitAll().build(); // lay tat ca cac quyen
+                StrictMode.setThreadPolicy(policy); // thiet lap chinh sac ket noi bao gom tat ca cac quyen
+                Class.forName("net.sourceforge.jtds.jdbc.Driver").newInstance();
+                connection = DriverManager.getConnection(sql);
+                Log.i("THONG BAO:","Ket noi thanh cong");
+            } catch (Exception e) {
+                Log.e("THONGBAO","LOSS");
+                throw new RuntimeException(e);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return connection;
+    }
+}
